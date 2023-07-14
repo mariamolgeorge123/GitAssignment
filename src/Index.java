23fe7c1c798f79a3794a9f58a1c4c096782501d0
mariamolgeorge123 <mariadaisy1990@gmail.com>
2023-07-14 15:10:27 -0400

    add code for sum

--- src/Index.java
+++ src/Index.java
@@ -3,5 +3,8 @@ import java.sql.SQLOutput;
 public class Index {
     public static void main(String[] args) {
         System.out.println("this is the assignment");
+        int x=10;
+        int y=20;
+        System.out.println("sum: "+(x+y));
     }
 }
